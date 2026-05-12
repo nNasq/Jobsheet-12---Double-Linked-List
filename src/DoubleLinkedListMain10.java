0e2de92e75a19cb7652e90a7c1e8c42969eea708
nNasq <hafizharrasyiids@gmail.com.>
2026-05-12 17:19:37 +0700

    1

--- src/DoubleLinkedListMain10.java
+++ src/DoubleLinkedListMain10.java
@@ -27,6 +27,7 @@ public class DoubleLinkedListMain10 {
             System.out.println("4. Hapus data di awal");
             System.out.println("5. Hapus data di akhir");
             System.out.println("6. Tampilkan data");
+            System.out.println("7. Reverse data");
             System.out.println("0. Keluar");
             System.out.print("Pilih menu : ");
             pilihan = Integer.parseInt(scan.nextLine());
@@ -57,6 +58,9 @@ public class DoubleLinkedListMain10 {
                     break;
                 case 6:
                     list.print();
+                    break;
+                case 7:
+                    list.printReverse();
                     break;
                 case 0:
                     System.out.println("Program selesai.");
